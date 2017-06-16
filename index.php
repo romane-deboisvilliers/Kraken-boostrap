c9588766b97a4bfdd1762bd465a9c12e32ec6885
romane-deboisvilliers <romane-deboisvilliers@hotmail.fr>
2017-06-16 20:55:56 +0400

    Ajout des dossiers dans le dépôt.

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Train</title>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbarPerso">
+     <div class="container-fluid">
+       <!-- Brand and toggle get grouped for better mobile display -->
+       <div class="navbar-header">
+         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo"></a>
+       </div>
+
+       <!-- Collect the nav links, forms, and other content for toggling -->
+       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+         <ul class="nav navbar-nav navbar-right">
+           <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-danger">SEARCH</button>
+          </form>
+         </ul>
+
+         <div class="menu">
+            <ul class="nav navbar-nav">
+               <li><a href="#">HOMEPAGE <br><small>test text here</small></a></li>
+               <li><a href="#">STYLE DEMO <br><small>test text here</small></a></li>
+               <li><a href="#">FULL WIDTH <br><small>test text here</small></a></li>
+               <li><a href="#">PORTFOLIO <br><small>test text here</small></a></li>
+               <li><a href="#">GALLERY <br><small>test text here</small></a></li>
+               <li><a href="#">DROPDOWN <br><small>test text here</small></a></li>
+            </ul>
+         </div>
+         <div class="nav navbar-nav navbar-right menuSocial">
+           <ul class="nav navbar-nav">
+              <li><a href="#"><img src="img/social_in.png" alt="logo"></a></li>
+              <li><a href="#"><img src="img/social_tw.png" alt="logo"></a></li>
+              <li><a href="#"><img src="img/social_pin.png" alt="logo"></a></li>
+              <li><a href="#"><img src="img/social_go.png" alt="logo"></a></li>
+              <li><a href="#"><img src="img/social_fl.png" alt="logo"></a></li>
+           </ul>
+         </div>
+       </div><!-- /.navbar-collapse -->
+
+     </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="container-fluid blocDiapo">
+      <div class="diapo">
+      </div>
+      <p class="legendeDiapo">Lorem ipsum sit dolor amet. Lorem ipsum sit dolor amet. Lorem ipsum sit dolor amet.</p>
+      <div class="point">
+        <ul class="">
+           <li class="pointAlign"><a href="#"><img src="img/point.png" alt="point"></a></li>
+           <li class="pointAlign"><a href="#"><img src="img/point.png" alt="point"></a></li>
+           <li class="pointAlign"><a href="#"><img src="img/point.png" alt="point"></a></li>
+           <li class="pointAlign"><a href="#"><img src="img/point.png" alt="point"></a></li>
+           <li class="pointAlign"><a href="#"><img src="img/point.png" alt="point"></a></li>
+        </ul>
+      </div>
+    </div><!-- /.container -->
+
+    <div class="container blocPresent">
+      <h1>Lorem ipsum sit dolor amet.</h1>
+      <small>Lorem ipsum sit dolor amet.Lorem ipsum sit dolor amet.</small>
+      <div class="row">
+          <div class="col-sm-6 col-md-3">
+            <div class="thumbnail">
+              <img src="img/bg.jpg" alt="perso">
+              <div class="caption">
+                <h3>John Doe</h3>
+                <p><a href="#" class="btn btn-danger" role="button">View Profile</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-md-3">
+            <div class="thumbnail">
+              <img src="img/bg.jpg" alt="perso">
+              <div class="caption">
+                <h3>John Doe</h3>
+                <p><a href="#" class="btn btn-danger" role="button">View Profile</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-md-3">
+            <div class="thumbnail">
+              <img src="img/bg.jpg" alt="perso">
+              <div class="caption">
+                <h3>John Doe</h3>
+                <p><a href="#" class="btn btn-danger" role="button">View Profile</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-md-3">
+            <div class="thumbnail">
+              <img src="img/bg.jpg" alt="perso">
+              <div class="caption">
+                <h3>John Doe</h3>
+                <p><a href="#" class="btn btn-danger" role="button">View Profile</a></p>
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+
+    <hr class="separation">
+
+    <div class="container blocProject">
+
+      <div class="col-sm-6 col-md-3">
+         <p>OUR LATEST</p>
+         <h2>PROJECT <br>WORK >></h2>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="img/bg.jpg" alt="perso">
+          <div class="caption">
+            <h3>YOUR TITLE</h3>
+            <p>Lorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor amet</p>
+            <p><a href="#" class="btn btn-danger" role="button">READ MORE</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="img/bg.jpg" alt="perso">
+          <div class="caption">
+            <h3>YOUR TITLE</h3>
+            <p>Lorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor amet</p>
+            <p><a href="#" class="btn btn-danger" role="button">READ MORE</a></p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <footer class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6 col-md-4">
+          <h3>ABOUT US</h3>
+          <p>Lorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor amet</p>
+          <a href="#">Read More>></a>
+
+          <h3>FROM THE BLOG</h3>
+          <p>Lorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor amet</p>
+          <a href="#">Read More>></a>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <h3>LINK BLOCK</h3>
+          <ul>
+            <li>lorem ipsum dolor sit</li>
+            <li>lorem ipsum dolor sit</li>
+            <li>lorem ipsum dolor sit</li>
+            <li>lorem ipsum dolor sit</li>
+            <li>lorem ipsum dolor sit</li>
+            <li>lorem ipsum dolor sit</li>
+            <li>lorem ipsum dolor sit</li>
+          </ul>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <h3>NEWSLETTER</h3>
+          <p>Lorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor ametLorem ipsum sit dolor amet.Lorem ipsum sit dolor amet</p>
+          <form class="navbar-form navbar-left">
+           <div class="form-group">
+             <input type="text" class="form-control" placeholder="Name"> <br><br>
+             <input type="text" class="form-control" placeholder="Email">
+           </div> <br><br>
+           <button type="submit" class="btn btn-danger">SUBMIT</button>
+         </form>
+         <div>
+           <h3>CONTACT US</h3>
+           <p>Tel: xxxxxxxxxxxxx</p>
+           <p>Email: xxxx@xxxxx.com</p>
+         </div> 
+
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
